65ef2a85f5068a5d148bd294bcb9fea9e032d7f4
dinhnv-vnext <30885230+dinhnv-vnext@users.noreply.github.com>
2018-03-27 18:33:40 +0700

    cmit

---
+++ resources/views/frontend/includes/header.blade.php
@@ -0,0 +1,6 @@
+<div id="header" class="row">
+    <div id="header-top" class="row">
+        @include('frontend.includes.lang-switcher')
+        @include('frontend.includes.account-menu')
+    </div>
+</div>
